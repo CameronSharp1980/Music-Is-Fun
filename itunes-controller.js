8043d3ad321493cd0a13b0adb065bcb11b04453d
Cameron Sharp <cameron.sharp1980@gmail.com>
2017-11-01 13:28:13 -0600

    Functionality complete. Moving on to styling

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,18 +1,38 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
-  function draw(){
+  function draw(songList) { //rename songList?
+    console.log(songList) //Look over the objects before you begin
+    var template = ''
+    for (var i = 0; i < songList.length; i++) {
+      var song = songList[i];
+      template +=
+        `
+      <div class="col-sm-12 col-md-6 col-lg-3 thumbnail">
+        <div>
+            <img src="${song.albumArt}" alt="Song Image">
+            <h4>${song.artist}</h4>
+            <h5>${song.collection}</h5>
+            <h5>${song.title}</h5>
+            <h6>${song.price}</h6>
+            <audio controls src="${song.preview}" preload="auto"></audio>
+
+        </div>
+      </div>
+      `
+    }
+    document.getElementById("songs").innerHTML = template
   }
 
 
 
 
-  
+
 }
